516c2c9e52d512d5ceae1427c6aad6894df32166
WEIUIJONG <dnldmlwhd@ajou.ac.kr>
2025-10-20 20:16:18 +0900

    feat($calculator): implement exception handling for negative or non-numeric input

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -10,14 +10,27 @@ class StringCalculator {
         const customDelimiterMatch = text.match(/^\/\/(.)\n(.*)/);
         if (customDelimiterMatch) {
             const customDelimiter = customDelimiterMatch[1];
-
             delimiter = new RegExp(this.escapeRegExp(customDelimiter));
             numbersText = customDelimiterMatch[2];
         }
 
         const numbers = numbersText.split(delimiter).map(Number);
 
+        // Validate the numbers after parsing
+        this.validateNumbers(numbers);
+
         return numbers.reduce((sum, num) => sum + num, 0);
+    }
+
+    validateNumbers(numbers) {
+        const negatives = numbers.filter((num) => num < 0);
+        if (negatives.length > 0) {
+            throw new Error('[ERROR] Negative numbers are not allowed.');
+        }
+
+        if (numbers.some(isNaN)) {
+            throw new Error('[ERROR] Invalid number format.');
+        }
     }
 
     escapeRegExp(string) {
